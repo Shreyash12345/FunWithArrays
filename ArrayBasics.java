2240a927bda0a0d83ede443809293d4da7860a9e
Shreyash12345 <102417739+Shreyash12345@users.noreply.github.com>
2022-04-02 13:28:04 +0100

    My First Time Using Arrays

---
+++ ArrayBasics.java
@@ -0,0 +1,14 @@
+class ArrayBasics {
+  public static void main(String[] args) {
+    double[]data=new double[21];//how many integers are in data
+    //initialising an array
+    double[]age={12,4,5,2,5};//the ages
+    data[4]=0;//data 4 equals zero
+    data[5]=9;//data 5 equals nine
+    data[15]=8;//data 15 equals eight
+    data[4]=data[5]/data[15];//dividing data 5 and data 15
+    System.out.println(data[4]);//printing value in array
+    
+    
+  }
+}
