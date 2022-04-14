ea1c6c1b40fe0f8f37286929a8011d6e8d38e7c6
Shreyash12345 <102417739+Shreyash12345@users.noreply.github.com>
2022-04-14 17:58:50 +0100

    Arrays Part 2

---
+++ Main (2).java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    int[] data={5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100};
+    for(int i=0;i<20;i++){
+     System.out.println(data[i]);
+  } 
+    }
+    
+  }
